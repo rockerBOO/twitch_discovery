2629124ca5061ce2730d14dc11d52cac818f0169
rockerBOO <rockerboo@gmail.com>
2015-07-18 19:58:23 -0400

    Put in css not js

--- brunch-config.js
+++ brunch-config.js
@@ -20,7 +20,10 @@ exports.config = {
       }
     },
     stylesheets: {
-      joinTo: 'css/app.css'
+      joinTo: {
+       'css/app.css': /^(web\/static\/css)/,
+       'css/vendor.css': /^(web\/static\/vendor\/css)/
+      },
     },
     templates: {
       joinTo: 'js/app.js'
