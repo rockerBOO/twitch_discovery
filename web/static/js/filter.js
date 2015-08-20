0a5ebad817b2fb4be67b3eae9f96d01ca2784f42
rockerBOO <rockerboo@gmail.com>
2015-08-20 12:16:23 -0400

    Updating MUI, PushState upated, Broken Images are replaced with default. Heights are now set on each stream block

--- web/static/js/filter.js
+++ web/static/js/filter.js
@@ -1,6 +1,45 @@
 function Filter() {}
 
-Filter.prototype.filter = function (params) {
+Filter.prototype.getStreams = function () {
+  var params = $('#filter_form').serialize();
+
+  // console.log(params)
+  // console.log("/api/streams?"+params)
+  jQuery.getJSON("/api/streams?"+params, function (data) {
+    var items = []
+    $.each(data, function(key, val) {
+      // console.log(key, val)
+        var block = '<div class="stream-block mui-col-md-4">'
+            + '<div class="stream-image">'
+            +  '<a href="'+val["channel"]["url"]+'">'
+            +    '<img src="'+val["preview"]["large"]+'" onerror="imgError(this);">'
+            +  '</a>'
+            +  '<div>'
+            +    '<a onclick="activatePreview(this);" class="preview" href="javascript:;"'
+            +      'data-channel="'+val["channel"]["name"]+'"'
+            +      'data-title="'+val["channel"]["status"]+'"'
+            +      'data-display-name="'+val["channel"]["display_name"]+'"'
+            +      'data-game="'+val["channel"]["game"]+'"'
+            +      'data-viewers="'+val["viewers"]+'"'
+            +      'data-followers="'+val["channel"]["followers"]+'"'
+            +      'data-views="'+val["channel"]["views"]+'"'
+            +    '><i class="fa fa-eye"></i> Preview</a>'
+            +  '</div>'
+            +'</div>'
+
+            +'<div class="stream_meta">'
+            +  '<h4><a href="'+val["channel"]["url"]+'" title="'+val["channel"]["status"]+'">'+val["channel"]["status"]+'</a></h4>'
+            +  '<span class="views">'+val["viewers"]+' viewers </span>'
+            +  '<span class="name">on <a href="'+val["channel"]["url"]+'">'+val["channel"]["display_name"]+'</a></span>'
+            + '</div>'
+          +'</div>';
+
+        items.push(block)
+    })
+
+    $("#stream-blocks").html(items.join(""))
+    history.pushState(params, "Streams - Discovery", "/streams?"+params);
+  })
 }
 
 module.exports = Filter;
--- web/static/js/onload.js
+++ web/static/js/onload.js
@@ -1,31 +1,13 @@
 
-$("a.preview").click(function(event) {
-  event.preventDefault()
+// $("a.preview").click(function(event) {
+//   console.log("Activating preview")
+//   event.preventDefault()
 
-  var channel       = event.currentTarget.getAttribute("data-channel")
-  var title         = event.currentTarget.getAttribute("data-title")
-  var display_name  = event.currentTarget.getAttribute("data-display-name")
-  var game          = event.currentTarget.getAttribute("data-game")
-  var followers     = event.currentTarget.getAttribute("data-followers")
-  var viewers       = event.currentTarget.getAttribute("data-viewers")
-  var views         = event.currentTarget.getAttribute("data-views")
+//   activatePreview(event.currentTarget)
+// });
 
-  Player.clearPreview()
-
-  Player.twitchPlayer(channel)
-  Player.twitchChat(channel)
-  Player.twitchMeta({
-    channel: channel,
-    title: title,
-    display_name: display_name,
-    game: game,
-    viewers: viewers,
-    followers: followers,
-    views: views
-  })
-
-  document.getElementById("preview_video").parentNode.parentNode.classList.remove("preview_off")
-});
+var filter = require('web/static/js/filter')
+var Filter = new filter()
 
 $("#search-fps-toggle").click(function (event) {
   $("#search-fps-options").toggleClass("hide-options")
@@ -42,55 +24,11 @@ $("#games-search-input").autocomplete({
 });
 
 $("#filter_form :input").change(function(event) {
-  getStreams()
+  Filter.getStreams()
 });
 
 $("#filter_form").submit(function (event) {
   event.preventDefault()
 
-  getStreams()
+  Filter.getStreams()
 });
-
-function getStreams() {
-  var params = $('#filter_form').serialize();
-
-  // console.log(params)
-  // console.log("/api/streams?"+params)
-  jQuery.getJSON("/api/streams?"+params, function (data) {
-    var items = []
-    $.each(data, function(key, val) {
-      // console.log(key, val)
-        var block = '<div class="stream-block mui-col-md-4">'
-            + '<div class="stream-image">'
-            +  '<a href="'+val["channel"]["url"]+'">'
-            +    '<img src="'+val["preview"]["large"]+'">'
-            +  '</a>'
-            +  '<div>'
-            +    '<a class="preview" href="javascript:;"'
-            +      'data-channel="'+val["channel"]["name"]+'"'
-            +      'data-title="'+val["channel"]["status"]+'"'
-            +      'data-display-name="'+val["channel"]["display_name"]+'"'
-            +      'data-game="'+val["channel"]["game"]+'"'
-            +      'data-viewers="'+val["viewers"]+'"'
-            +      'data-followers="'+val["channel"]["followers"]+'"'
-            +      'data-views="'+val["channel"]["views"]+'"'
-            +    '><i class="fa fa-eye"></i>Preview</a>'
-            +  '</div>'
-            +'</div>'
-
-            +'<div class="stream_meta">'
-            +  '<h4><a href="'+val["channel"]["url"]+'" title="'+val["channel"]["status"]+'">'+val["channel"]["status"]+'</a></h4>'
-            +  '<span class="views">'+val["viewers"]+' viewers</span>'
-            +  '<span class="name">on <a href="'+val["channel"]["url"]+'">'+val["channel"]["display_name"]+'</a></span>'
-            + '</div>'
-          +'</div>';
-
-        items.push(block)
-    })
-
-    // console.log(items.join(""))
-
-    $("#stream-blocks").html(items.join(""))
-    // $("#stream-blocks").appendTo(  );
-  })
-}
